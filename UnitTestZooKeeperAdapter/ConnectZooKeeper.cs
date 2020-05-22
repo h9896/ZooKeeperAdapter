6b845f8afe757efcb58da5aaf38c9a7bd3bc53eb
edison <h123453047@gmail.com>
2020-05-22 14:17:46 +0800

    Add UnitTest

---
+++ UnitTestZooKeeperAdapter/ConnectZooKeeper.cs
@@ -0,0 +1,68 @@
+﻿using NUnit.Framework;
+using ZooKeeperNet;
+using System;
+using System.Threading.Tasks;
+using System.Threading;
+using ZookeeperAdapter;
+
+namespace UnitTestZooKeeperAdapter
+{
+    [TestFixture]
+    public class ConnectZooKeeper
+    {
+        [Test, Category("NonSyncConnected")]
+        public void NonSyncConnected()
+        {
+            ZooKeeperAdapter adapter = GetAdapter();
+            CountdownEvent eventStart = new CountdownEvent(1);
+            CountdownEvent mainStart = new CountdownEvent(1);
+            Task.Run(() => { eventStart.Wait(2000); adapter.Process(GetConnectEvent()); });
+            adapter.NonSyncConnected();
+            eventStart.Signal();
+            mainStart.Wait(1000);
+            Assert.AreEqual(adapter.ConnectState, true);
+        }
+        [Test, Category("SyncConnected")]
+        public void SyncConnected()
+        {
+            ZooKeeperAdapter adapter = GetAdapter();
+            CountdownEvent eventStart = new CountdownEvent(1);
+            Task.Run(() => { eventStart.Wait(2000); adapter.Process(GetConnectEvent()); });
+            adapter.SyncConnected();
+            Assert.AreEqual(adapter.ConnectState, true);
+        }
+        [Test, Category("FailNonSyncConnected")]
+        public void FailNonSyncConnected()
+        {
+            ZooKeeperAdapter adapter = GetAdapter();
+            CountdownEvent eventStart = new CountdownEvent(1);
+            CountdownEvent mainStart = new CountdownEvent(1);
+            Task.Run(() => { eventStart.Wait(2000); adapter.Process(GetNonConnectEvent()); });
+            adapter.NonSyncConnected();
+            eventStart.Signal();
+            mainStart.Wait(1000);
+            Assert.AreEqual(adapter.ConnectState, false);
+        }
+        [Test, Category("FailSyncConnected")]
+        public void FailSyncConnected()
+        {
+            ZooKeeperAdapter adapter = GetAdapter();
+            CountdownEvent eventStart = new CountdownEvent(1);
+            Task.Run(() => { eventStart.Wait(2000); adapter.Process(GetNonConnectEvent()); });
+            adapter.SyncConnected();
+            Assert.AreEqual(adapter.ConnectState, false);
+        }
+        public WatchedEvent GetConnectEvent()
+        {
+            return new WatchedEvent(KeeperState.SyncConnected, EventType.None, "/Test");
+        }
+        public WatchedEvent GetNonConnectEvent()
+        {
+            return new WatchedEvent(KeeperState.Unknown, EventType.None, "/Test");
+        }
+        public ZooKeeperAdapter GetAdapter()
+        {
+            return new ZooKeeperAdapter("/Test", "127.0.0.1", new TimeSpan(0, 0, 1));
+        }
+    }
+}
--- ZooKeeperAdapter/ZookeeperAdapter.cs
+++ ZooKeeperAdapter/ZookeeperAdapter.cs
@@ -2,32 +2,63 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
+using System.Threading;
 using System.Threading.Tasks;
 using ZookeeperAdapter.Manage;
 using ZooKeeperNet;
 
 namespace ZookeeperAdapter
 {
-    public class ZooKeeperAdapter
+    public class ZooKeeperAdapter : IWatcher
     {
         public string RootPath { get; private set; }
         public string EndPoint { get; private set; }
+        public bool ConnectState { get; private set; }
         public Dictionary<string, Dictionary<string, string>> CareNode { get; private set; }
         static internal ZooKeeper _zk;
         private TimeSpan _timeout;
         private ManageFactory _factory = new ManageFactory();
+        private CountdownEvent _connectLock { get; set; }
+        private int _lockTimeout { get; set; }
 
         public ZooKeeperAdapter(string rootPath, string endPoint, TimeSpan sessionTimeout)
         {
             RootPath = rootPath;
             EndPoint = endPoint;
+            ConnectState = false;
             _timeout = sessionTimeout;
+            _lockTimeout = 5000;
+            _connectLock = new CountdownEvent(1);
             CareNode = new Dictionary<string, Dictionary<string, string>>();
+        }
+        // SyncConnected
+        public void SyncConnected()
+        {
+            if (!ConnectState)
+            {
+                _zk = new ZooKeeper(EndPoint, _timeout, this);
+                _connectLock.Wait(_lockTimeout);
+            }
+        }
+        // NonSyncConnected
+        public void NonSyncConnected()
+        {
+            if (!ConnectState) _zk = new ZooKeeper(EndPoint, _timeout, this);
+        }
+        public void Close()
+        {
+            try
+            {
+                _zk.Dispose();
+            }
+            catch (ThreadInterruptedException errors)
+            {
+                throw errors;
+            }
         }
         public void Initialize(string topicList)
         {
-            _zk = new ZooKeeper(EndPoint, _timeout, new WatchProcess());
-            foreach(string topic in topicList.Split('|'))
+            foreach (string topic in topicList.Split('|'))
             {
                 IZookeeperAdapter adapter = _factory.InitializeManage(topic);
                 adapter.itemHandler += (item) => UpdateItem(item, topic);
@@ -36,6 +67,14 @@ namespace ZookeeperAdapter
         private void UpdateItem(Dictionary<string, string> item, string topic)
         {
             CareNode[topic] = item;
+        }
+        public void Process(WatchedEvent @event)
+        {
+            if (@event.State == KeeperState.SyncConnected)
+            {
+                ConnectState = true;
+                _connectLock.Signal();
+            }
         }
     }
 }
